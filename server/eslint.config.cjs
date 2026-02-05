bd7ff3c31f6c6717075fad377166ebd02c75fc60
Lyyrai <lyyrai@icloud.com>
2026-02-05 09:20:46 +0200

    Add ESLint tsconfig to include root config

--- server/eslint.config.cjs
+++ server/eslint.config.cjs
@@ -13,7 +13,7 @@ module.exports = [
         languageOptions: {
             parser: tsParser,
             parserOptions: {
-                project: './tsconfig.json',
+                project: './tsconfig.eslint.json',
                 tsconfigRootDir: __dirname
             },
             ecmaVersion: 2024,
